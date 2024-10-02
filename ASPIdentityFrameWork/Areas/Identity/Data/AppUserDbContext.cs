e8ceba3d864ea1d9fa92e8f9caf0928988d1da93
sarmadalij <developer.sarmadali@gmail.com>
2024-10-02 15:27:53 +0500

    Add project files.

---
+++ ASPIdentityFrameWork/Areas/Identity/Data/AppUserDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ASPIdentityFrameWork.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ASPIdentityFrameWork.Areas.Identity.Data;
+
+public class AppUserDbContext : IdentityDbContext<AppUser>
+{
+    public AppUserDbContext(DbContextOptions<AppUserDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new AppUserEntityConfig());
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
+
+//for Config
+public class AppUserEntityConfig : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(255);
+        builder.Property(x => x.LastName).HasMaxLength(255);
+    }
+}
